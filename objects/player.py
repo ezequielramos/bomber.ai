28a3106bd74a55656fe7f76b14663d2d6252f083
Ezequiel Ramos <ezequielmr94@gmail.com>
2019-07-14 17:54:16 -0300

    created player object

---
+++ objects/player.py
@@ -0,0 +1,5 @@
+class Player(object):
+    def __init__(self):
+        self.points = 0
+        self.bombs_limit = 1 #TODO: use this property to valid number of bombs
+        #TODO: propriedades para os poderes
